b54200366d281fa7e11fd0d6de37c869d03da486
mfhumam <mfawwazhumam@students.amikom.ac.id>
2023-04-06 06:32:08 +0700

    ngumpulin tugas

---
+++ pemrograman/TugasClassTaxi-master/TugasClassTaxi-master/TugasClassTaxi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new TugasClassTaxi.Taxi();
+
+            taxi.DriverName = "jarwo";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10 ;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
